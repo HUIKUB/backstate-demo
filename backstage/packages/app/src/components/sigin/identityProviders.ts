4136a2e055bddb1cca88592a4fce726397fc3350
Poundpitak Amornlerdlittichai <206996520+HUIKUB@users.noreply.github.com>
2025-10-29 17:11:36 +0700

    feat: add ssp github 💸

---
+++ backstage/packages/app/src/components/sigin/identityProviders.ts
@@ -0,0 +1,10 @@
+// import { githubAuthApiRef } from '@backstage/core-plugin-api';
+
+export const providers = [
+  {
+    id: 'github',
+    title: 'GitHub',
+    message: 'Sign in using GitHub',
+    // apiRef: githubAuthApiRef,
+  },
+];
---
+++ backstage/packages/backend/src/plugins/kubernetes.ts
@@ -0,0 +1,10 @@
+// import { Router } from 'express';
+// import { PluginEnvironment } from '../types';
+// import { createRouter } from '@backstage/plugin-kubernetes-backend';
+
+// export default async function createPlugin(env: PluginEnvironment): Promise<Router> {
+//   return await createRouter({
+//     logger: env.logger,
+//     config: env.config,
+//   });
+// }
